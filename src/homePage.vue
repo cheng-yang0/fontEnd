4c243e49635304463ce81c2683da4de44287a4b9
e <chengyang@linkdoc.com>
2021-02-02 14:43:19 +0800

    新增几个文件

---
+++ src/homePage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        首页
+    </div>
+</template>
+<script>
+    import axios from './axios.js'
+    import {message} from 'ant-design-vue'
+    import {Subject} from 'rxjs'
+    export default {
+        created(){
+            const subject=new Subject()
+            let a=1
+            subject.subscribe(val=>console.log(a))
+            a=2
+            subject.subscribe(val=>console.log(a))
+            subject.next('')
+        }
+    }
+</script>
